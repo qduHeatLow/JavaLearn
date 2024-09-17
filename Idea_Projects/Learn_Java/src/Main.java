084ae56a9bef68567e3671b64a5e0e334356f66d
HeatLow <1442984671@qq.com>
2024-09-17 17:42:06 +0800

    java 学习0917

--- Idea_Projects/Learn_Java/src/Main.java
+++ Idea_Projects/Learn_Java/src/Main.java
@@ -1,20 +1,12 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main{
 
     public static void main(String[] args) {
-        ParameterizedType genericSuperclass = (ParameterizedType) ArrayList.class.getGenericSuperclass();
-        Type[] actualTypeArguments = genericSuperclass.getActualTypeArguments();
-        for (Type actualTypeArgument : actualTypeArguments) {
-            System.out.println(actualTypeArgument);
-        }
+        int a = 1;
+        a++;
+
     }
 
 
